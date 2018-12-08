f0389ba0d18d38b0425e8aaf750336b9912367fd
Taylor Quinn <tquinn@sagacom.com>
2018-12-08 14:29:57 -0500

    add uft code and color place holders

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def gimme_string(chunk):
 def generate_map(x, y):
     campfire_map = []
 
-    for i in range(x*y):
+    for i in range(x * y):
         campfire_map.append(choice(surfaces))
 
     return campfire_map
@@ -44,7 +44,7 @@ def display_map(mappy):
     return graphy
 
 
-constant_map = generate_map(100,50)
+constant_map = generate_map(100, 50)
 # print(constant_map);
 
 window = pyglet.window.Window()
@@ -52,31 +52,28 @@ window = pyglet.window.Window()
 music = pyglet.resource.media('assets/music/temp-music.mp3')
 music.play()
 
-
 batch = pyglet.graphics.Batch()
 
 label = pyglet.text.Label('Campfire Dad Sim',
                           font_name='Times New Roman',
                           font_size=36,
-                          x=window.width//2, y=window.height//2,
+                          x=window.width // 2, y=window.height // 2,
                           anchor_x='center', anchor_y='center')
 
 grass_image = pyglet.image.load('assets/sprites/environment/grass.gif')
 grass = pyglet.sprite.Sprite(grass_image, x=64, y=64)
 
-
-
 grass_sprites = []
 for i in range(4):
-    x, y = i+1 * 64, 64
+    x, y = i + 1 * 64, 64
     grass_sprites.append(pyglet.sprite.Sprite(grass_image, x, y, batch=batch))
+
 
 @window.event
 def on_draw():
     window.clear()
     label.draw()
     batch.draw()
-
 
 
 pyglet.app.run()
--- assets.py
+++ assets.py
@@ -1,5 +1,9 @@
 surfaces = {
-0:{'type': 'grass', 'img': 'assets/sprites/environment/grass.gif', 'speed': 2, 'reward': 0, 'probability': 10},
-1:{'type': 'forest', 'img': 'assets/sprites/environment/forest.gif', 'speed': 1, 'reward': 0.5, 'probability': 5},
-2:{'type': 'log', 'img': 'assets/sprites/environment/log.gif', 'speed': 0.5, 'reward': 1, 'probability': 3},
-3:{'type': 'water', 'img': 'assets/sprites/environment/water.gif', 'speed': 0, 'reward': 0, 'probability': 5}}
+    0: {'type': 'grass', 'utf': {'code': '9608', 'color': '#eee'}, 'image': 'assets/sprites/environment/grass.gif',
+        'speed': 2, 'reward': 0, 'probability': 10},
+    1: {'type': 'forest', 'utf': {'code': '9608', 'color': '#eee'}, 'image': 'assets/sprites/environment/forest.gif',
+        'speed': 1, 'reward': 0.5, 'probability': 5},
+    2: {'type': 'log', 'utf': {'code': '9608', 'color': '#eee'}, 'image': 'assets/sprites/environment/log.gif',
+        'speed': 0.5, 'reward': 1, 'probability': 3},
+    3: {'type': 'water', 'utf': {'code': '9608', 'color': '#eee'}, 'image': 'assets/sprites/environment/water.gif',
+        'speed': 0, 'reward': 0, 'probability': 5}}
